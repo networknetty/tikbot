1aa453a96269167e93861f686b1512ede3ba554a
Volodymyr Romantsev <volodymyr.romantsev@everymatrix.com>
2025-03-02 16:31:49 +0200

    tik worked

--- index.js
+++ index.js
@@ -1,66 +1,86 @@
 
-const { Telegraf } = require("telegraf");
-const axios = require("axios");
-const fs = require("fs");
+const { Telegraf } = require("telegraf")
+const axios = require("axios")
+const fs = require("fs")
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => {
-  ctx.reply("waiting for TikTok");
-});
+bot.start((ctx) => ctx.reply('hello!'))
+
+const apis = ['https://saveig.app', 'https://snapinsta.app', 'https://igram.io']
+async function tryInstApi(ctx, message) {
+  const _apis = [...apis],
+    eArr = []
+  while (_apis.length) {
+    try {
+      const api = _apis.shift()
+      const res = await axios.get(`${api}/api?url=${message}`)
+      for (const media of res.data.media) {
+        media.type === "video" ?
+          await ctx.replyWithVideo({ url: media.url }) :
+          await ctx.replyWithPhoto({ url: media.url })
+      }
+      return
+    }
+    catch(err){
+      eArr.push(err)
+    }
+  }
+  return Promise.reject(eArr)
+}
 
 bot.on("text", async (ctx) => {
-  const message = ctx.message.text;
-  const tiktokUrlRegex = /(https?:\/\/www\.tiktok\.com\/[^\s]+)/;
-
-  if (tiktokUrlRegex.test(message)) {
-    ctx.reply("loading...");
-
+  const message = ctx.message.text
+  if (message.includes("tiktok.com")) {
     try {
-      const apiUrl = `https://www.tikwm.com/api/?url=${message}`;
-      const response = await axios.get(apiUrl);
-      const videoUrl = response.data?.data?.play;
-
+      const apiUrl = `https://www.tikwm.com/api/?url=${message}`
+      const response = await axios.get(apiUrl)
+      const videoUrl = response.data?.data?.play
       if (videoUrl) {
-        const videoPath = "tiktok_video.mp4";
-        const videoStream = fs.createWriteStream(videoPath);
-
+        const videoPath = "tiktok_video.mp4"
+        const videoStream = fs.createWriteStream(videoPath)
         const videoResponse = await axios({
           url: videoUrl,
           method: "GET",
           responseType: "stream",
-        });
-
-        videoResponse.data.pipe(videoStream);
-
+        })
+        videoResponse.data.pipe(videoStream)
         videoStream.on("finish", async () => {
-          await ctx.replyWithVideo({ source: videoPath });
-          fs.unlinkSync(videoPath); // Удаляем видео после отправки
-        });
+          await ctx.replyWithVideo({ source: videoPath })
+          fs.unlinkSync(videoPath) // Удаляем видео после отправки
+        })
       } else {
-        ctx.reply("fail");
+        console.error("fail videoUrl")
+        // ctx.reply("fail videoUrl")
+        ctx.reply(`❌`)
       }
     } catch (error) {
-      console.error(error);
-      ctx.reply("fail... try later");
+      console.error(error)
+      // ctx.reply("fail... try later")
+      ctx.reply(`❌ ${error}`)
     }
-  } else {
-    ctx.reply("error link TikTok!");
   }
-});
+  else if (message.includes("instagram.com")) {
+    try {
+      await tryInstApi(ctx, message)
+    }
+    catch (error) {
+      // ctx.reply(`❌ ${error}`)
+      console.error(`❌ ${error}`)
+    }
+  }
+  else {
+    // ctx.reply("without TikTok / Instagram!")
+  }
+})
 
-bot.launch();
-console.log(" bot serve...");
+bot.launch()
 
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
 
-// Заглушка для Render (он требует открытый порт)
-app.get("/", (req, res) => {
-  res.send("app started!");
-});
+app.get("/", (req, res) =>
+  res.send("express get/"))
 
-app.listen(PORT, () => {
-  console.log(`faik start server port: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`start express port: ${PORT}`))
